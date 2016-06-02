98a8a6decaae00be4fed2c175b8a12f0b29328ea
nicolastarzia <git@nicolastarzia.com>
2016-06-02 17:05:01 -0300

    Add Evernote backup

---
+++ Powershell/BackupEvernoteFiles.ps1
@@ -0,0 +1,10 @@
+# 
+#	Why I wrote this: 
+#	Backup all my Evernote files to my own filesystem
+#
+
+$defaultDir = "E:\my\BkpEvernote\"
+$evernotePath="C:\Program Files (x86)\Evernote\Evernote\ENScript.exe"
+$defaultDate = "{0}" -f (Get-Date -format M.d.yyyy)
+$pathExport = "{0}EvernoteNicolas.{1}.enex" -f $defaultDir, $defaultDate
+& $evernotePath exportNotes /q any: /f $pathExport
